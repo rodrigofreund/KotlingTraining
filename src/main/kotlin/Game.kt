b8ddedc6ff0c9106e27f887e88853558c1df8261
Rodrigo Freund de Moraes <rodrigofreund@gmail.com>
2024-10-21 12:23:02 +0100

    readme update

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -5,10 +5,8 @@ class Game(
     private val title: String,
     private val cover: String) {
 
-    val description = ""
-
     override fun toString(): String {
-        return "Game(title='$title', cover='$cover', description='$description')"
+        return "Game(title='$title', cover='$cover')"
     }
 
 
